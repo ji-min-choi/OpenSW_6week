45009808910b417eb4779668f7409b772d46fa23
ji-min-choi <4metree@naver.com>
2023-04-16 22:20:13 +0900

    오픈소스전문프로젝트 6주차 미션9

---
+++ app/src/main/java/com/example/mission9/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mission9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// 2021041083 최지민
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
