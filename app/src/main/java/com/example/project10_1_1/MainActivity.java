cb508ffeeb57d10c076c781dbf29072a8cd293ea
ji-min-choi <4metree@naver.com>
2023-04-16 21:34:16 +0900

    오픈소스전문프로젝트 6주차 실습 10-1

---
+++ app/src/main/java/com/example/project10_1_1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.project10_1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+// 2021041083 최지민
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
